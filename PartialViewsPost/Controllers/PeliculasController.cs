46adce0b0a99f0a99d58a12d61e8c97564870591
attiaApp <attiapp123@gmail.com>
2022-03-04 18:57:54 +0100

    fgbh

---
+++ PartialViewsPost/Controllers/PeliculasController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewsPost.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialViewsPost.Controllers
+{
+    public class PeliculasController : Controller
+    {
+
+        private List<Pelicula> peliculas;
+
+        public PeliculasController()
+        {
+            this.peliculas = new List<Pelicula>
+            {
+                new Pelicula{ IdPelicula=1, Nombre="Alien",Genero="Ciencia ficcion"},
+                 new Pelicula{ IdPelicula=2, Nombre="Avatar",Genero="Ciencia ficcion"},
+                  new Pelicula{ IdPelicula=3, Nombre="Le Mans '66",Genero="Accion"},
+            };
+        }
+        public IActionResult Index()
+        {
+
+            
+            return View(this.peliculas);
+        }
+
+        public IActionResult _PeliculasPartial()
+        {
+            return PartialView("_PeliculasPartial", this.peliculas);
+        }
+    }
+}
---
+++ PartialViewsPost/Models/Pelicula.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialViewsPost.Models
+{
+    public class Pelicula
+    {
+        public int IdPelicula { get; set; }
+        public string Nombre { get; set; }
+        public string Genero { get; set; }
+
+    }
+}
